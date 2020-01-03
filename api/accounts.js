4a50792f5ff488a0b21e464cbe34d4b6fa61c174
Luka Bezovšek <luka.bezovsek@gmail.com>
2020-01-03 14:56:34 +0100

    User schema fix, cardId values are unique. Added other routes and error preventions.

--- api/accounts.js
+++ api/accounts.js
@@ -7,7 +7,8 @@ module.exports = function(server) {
 	/**
 	 * Login
 	 */
-	server.post('/login', (req, res, next) => {
+	server.post('/user/login', (req, res, next) => {
+		// TODO: integrate secure method for login (e.g. passport)
 		res.send(200, {'message': 'Login response'});
 		next();
 	});
@@ -16,7 +17,7 @@ module.exports = function(server) {
 	/**
 	 * SignUp
 	 */
-	server.post('/signup', (req, res, next) => {
+	server.post('/user/create', (req, res, next) => {
 		const data = req.body || {};
 
 		// validate data
@@ -27,27 +28,78 @@ module.exports = function(server) {
 			data.createdAt = new Date();
 			data.lastUpdatedAt = new Date();
 
-			//TODO: check if user with provided email already exists
-
-			console.log("DATA: ", data);
-
-/*			User.create(data, function (err, content) {
-				if (err) res.send(500, err);
-				// saved!
-
-				res.send(200, content);
-				next();
-			});*/
+			// TODO: for now mongoose schema takes care of email duplications - maybe check can be done at this level where message will be sent to the user
+			// TODO: password should be encrypted
 
 			User.create(data)
 				.then(usr => {
-					console.log("AAAAAAAAAAAAAAAAAA", usr);
 					res.send(200, usr);
 					next();
 				})
 				.catch(err => {
 					res.send(500, err);
-				})
+				});
 		}
+	});
+
+	/**
+	 * Get single user
+	 */
+	server.get('/user/:userId', (req, res, next) => {
+
+		if (!req.params.userId) {
+			res.send(500, {'message': 'Required parameter userId is missing.'});
+		}
+
+		User.findById(req.params.userId)
+			.then(user => {
+				if (!user) res.send(404, {'message': `User with id ${req.params.userId} was not found.`});
+				else res.send(200, user);
+				next();
+			})
+			.catch(err => {
+				res.send(500, err);
+			});
+	});
+
+	/**
+	 * Update user's account
+	 */
+	server.put('/user/:userId', (req, res, next) => {
+
+		if (!req.params.userId) {
+			res.send(500, {'message': 'Required parameter userId is missing.'});
+		}
+
+		let data = req.body || {};
+
+		User.update({ _id: req.params.userId}, data)
+			.then(user => {
+				res.send(200, user);
+				next();
+			})
+			.catch(err => {
+				res.send(500, err);
+			});
+	});
+
+	/**
+	 * Delete user's account
+	 */
+	server.del('/user/:userId', (req, res, next) => {
+
+		if (!req.params.userId) {
+			res.send(500, {'message': 'Required parameter userId is missing.'});
+		}
+
+		User.findOneAndRemove({ _id: req.params.userId })
+			.then(() => {
+				res.send(200, { 'message': `User with _id ${req.params.userId} has been deleted.`});
+				next();
+			})
+			.catch(err => {
+				res.send(500, err);
+			});
+
 	});
 };
