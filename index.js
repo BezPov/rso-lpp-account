086f52232918a886f536517c809b0a75b4198e77
Luka Bezovšek <luka.bezovsek@gmail.com>
2020-01-04 13:32:52 +0100

    Added cors middleware

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 const restify = require('restify');
 const mongoose = require('mongoose');
+const corsMiddleware = require('restify-cors-middleware');
+
+let cors = corsMiddleware({
+    preflightMaxAge: 5,
+    origins: ['*'],
+    allowHeaders:['X-App-Version'],
+    exposeHeaders:[]
+});
 
 // TODO: configuration should be moved to separate file
 const dbUri = 'mongodb+srv://bezoPovi:bezpov123!@lppcluster-hjwow.azure.mongodb.net/test?retryWrites=true&w=majority';
@@ -22,6 +30,9 @@ const server = restify.createServer({
 
 server.use(restify.plugins.queryParser());
 server.use(restify.plugins.bodyParser());
+
+server.pre(cors.preflight);
+server.use(cors.actual);
 
 server.get('/', (req, res, next) => {
     res.json({
